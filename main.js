9115d5a1246e6e86b3a7c22b75ef72200bfab297
RafaelDamasco <rafaeldamasco_@hotmail.com>
2022-05-11 13:10:45 -0300

    Desafio contando até 99

---
+++ main.js
@@ -0,0 +1,16 @@
+let contador = document.querySelector('#contador')
+contador.textContent = 1
+let c = 1
+
+const timer = setInterval(() => {
+  c ++
+  contador.textContent ++
+
+  if (c === 99) {
+    c = 1
+    contador.textContent = 1
+  }
+}, 1000)
+
+
+
